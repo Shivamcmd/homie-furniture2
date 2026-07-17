9b20647466ec43ab1a305f260caa6fae2f2d6e55
Shivam Sharma <shivamsharmaofficial09@gmail.com>
2026-07-17 22:28:40 +0530

    Fix asset paths

--- src/Components/NavbarComponents/Logo.jsx
+++ src/Components/NavbarComponents/Logo.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/Icons/favicon.png";
+import logo from "../../Assets/Icons/favicon.png";
 
 const Logo = () => {
   return (
