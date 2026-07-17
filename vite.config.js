d59680558eb6a450facd5574dec22406388134f8
Shivam Sharma <shivamsharmaofficial09@gmail.com>
2026-07-17 22:42:15 +0530

    Allow Render host

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react],
+  preview: {
+    allowedHosts: ["homie-furniture2-1.onrender.com"],
+  },
+});
