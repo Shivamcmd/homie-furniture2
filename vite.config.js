a4135a37a3cc54228eb4921055fa64a7d19bd2c6
Shivam Sharma <shivamsharmaofficial09@gmail.com>
2026-07-18 11:20:29 +0530

    Use plugin-react-swc

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [react],
+  plugins: [react()],
   preview: {
     allowedHosts: ["homie-furniture2-1.onrender.com"],
   },
